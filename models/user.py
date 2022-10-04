281e25ae945aecc8d45356813bc80a80adb5c93b
yasith <it19966922@my.sliit.lk>
2022-10-04 23:47:43 +0530

    create user model

---
+++ models/user.py
@@ -0,0 +1,40 @@
+class User:
+    def __init__(self, _id, username, password):
+        self.id = _id
+        self.username = username
+        self.password = password
+
+    def __str__(self):
+        return "User: " + self.username + " Password: " + self.password
+
+    @classmethod
+    def find_by_username(self, username):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM users WHERE username=?"
+        result = cursor.execute(query, (username,))
+        row = result.fetchone()
+        if row:
+            user = User(row[0], row[1], row[2])
+        else:
+            user = None
+
+        connection.close()
+        return user
+
+    @classmethod
+    def find_by_id(self, _id):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM users WHERE id=?"
+        result = cursor.execute(query, (_id,))
+        row = result.fetchone()
+        if row:
+            user = User(row[0], row[1], row[2])
+        else:
+            user = None
+
+        connection.close()
+        return user
